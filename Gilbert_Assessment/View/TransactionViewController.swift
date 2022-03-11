3de30bf16a9a587f5548f4aaccf5d071192b1e8d
Gilbert Nicholas Winata <gilbertnicholas09@gmail.com>
2022-03-11 19:11:14 +0700

    tableViewTran

--- Gilbert_Assessment/View/TransactionViewController.swift
+++ Gilbert_Assessment/View/TransactionViewController.swift
@@ -20,18 +20,21 @@ class TransactionViewController: UIViewController {
 
 }
 
-extension TransactionViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        <#code#>
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-    
-}
+//extension TransactionViewController: UITableViewDelegate, UITableViewDataSource {
+//    
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        //
+//        return 5
+//    }
+//    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        //
+//        return 5
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        //
+//        let cell = tableView
+//    }
+//    
+//}
