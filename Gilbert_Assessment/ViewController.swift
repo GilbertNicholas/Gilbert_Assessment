2129feb4d9c32aa99773c508a2cde9a1e22975c5
Gilbert Nicholas Winata <gilbertnicholas09@gmail.com>
2022-03-11 10:09:38 +0700

    Initial Commit

---
+++ Gilbert_Assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gilbert_Assessment
+//
+//  Created by Gilbert Nicholas on 11/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
